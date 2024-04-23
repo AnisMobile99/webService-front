d2a2e6deda0357295914a1b67ca13ecdd1dd4f38
AnisMobile99 <anis.salahbey.dz@gmail.com>
2024-04-23 17:33:20 +0200

    nettoyage

--- src/main.tsx
+++ src/main.tsx
@@ -27,200 +27,217 @@ import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import Coachs from "./views/Coaches.tsx";
-import User from "./views/User.tsx";
 
 const drawerWidth: number = 240;
 
 interface AppBarProps extends MuiAppBarProps {
-	open?: boolean;
+  open?: boolean;
 }
 
 const AppBar = styled(MuiAppBar, {
-	shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== "open",
 })<AppBarProps>(({ theme, open }) => ({
-	zIndex: theme.zIndex.drawer + 1,
-	transition: theme.transitions.create(["width", "margin"], {
-		easing: theme.transitions.easing.sharp,
-		duration: theme.transitions.duration.leavingScreen,
-	}),
-	...(open && {
-		marginLeft: drawerWidth,
-		width: `calc(100% - ${drawerWidth}px)`,
-		transition: theme.transitions.create(["width", "margin"], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.enteringScreen,
-		}),
-	}),
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
 }));
 
 const Drawer = styled(MuiDrawer, {
-	shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-	"& .MuiDrawer-paper": {
-		position: "relative",
-		whiteSpace: "nowrap",
-		width: drawerWidth,
-		transition: theme.transitions.create("width", {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.enteringScreen,
-		}),
-		boxSizing: "border-box",
-		...(!open && {
-			overflowX: "hidden",
-			transition: theme.transitions.create("width", {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.leavingScreen,
-			}),
-			width: theme.spacing(7),
-			[theme.breakpoints.up("sm")]: {
-				width: theme.spacing(9),
-			},
-		}),
-	},
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
 }));
 
 export const MainLayout: React.FC = () => {
-	const [open, setOpen] = useState(true);
-	const toggleDrawer = () => {
-		setOpen(!open);
-	};
-	const navigate = useNavigate();
+  const [open, setOpen] = useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+  const navigate = useNavigate();
 
-	return (
-		<Box sx={{ display: "flex" }}>
-			<CssBaseline />
-			<AppBar position="absolute" open={open}>
-				<Toolbar
-					sx={{
-						pr: "24px", // keep right padding when drawer closed
-					}}
-				>
-					<IconButton
-						edge="start"
-						color="inherit"
-						aria-label="open drawer"
-						onClick={toggleDrawer}
-						sx={{
-							marginRight: "36px",
-							...(open && { display: "none" }),
-						}}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography
-						component="h1"
-						variant="h6"
-						color="inherit"
-						noWrap
-						sx={{ flexGrow: 1 }}
-					>
-						Tableau de bord
-					</Typography>
-					<IconButton
-						color="inherit"
-						onClick={async () => {
-							await logOut();
-							navigate("/");
-						}}
-					>
-						<LogoutIcon />
-					</IconButton>
-				</Toolbar>
-			</AppBar>
-			<Drawer variant="permanent" open={open}>
-				<Toolbar
-					sx={{
-						display: "flex",
-						alignItems: "center",
-						justifyContent: "flex-end",
-						px: [1],
-					}}
-				>
-					<IconButton onClick={toggleDrawer}>
-						<ChevronLeftIcon />
-					</IconButton>
-				</Toolbar>
-				<Divider />
-				<List component="nav">
-					<NavLink to="/dashboard">
-						<ListItemButton>
-							<ListItemIcon>
-								<DashboardIcon />
-							</ListItemIcon>
-							<ListItemText primary="Tableau de bord" />
-						</ListItemButton>
-					</NavLink>
-					<NavLink to="/sportifs">
-						<ListItemButton>
-							<ListItemIcon>
-								<PeopleIcon />
-							</ListItemIcon>
-							<ListItemText primary="Sportifs" />
-						</ListItemButton>
-					</NavLink>
-					<NavLink to="/coachs">
-						<ListItemButton>
-							<ListItemIcon>
-								<FitnessCenterIcon />
-							</ListItemIcon>
-							<ListItemText primary="Coachs" />
-						</ListItemButton>
-					</NavLink>
-				</List>
-			</Drawer>
-			<Box
-				component="main"
-				sx={{
-					backgroundColor: (theme) =>
-						theme.palette.mode === "light"
-							? theme.palette.grey[100]
-							: theme.palette.grey[900],
-					flexGrow: 1,
-					height: "100vh",
-					overflow: "auto",
-				}}
-			>
-				<Toolbar />
-				<Box sx={{ m: 4 }}>
-					<Outlet />
-				</Box>
-			</Box>
-		</Box>
-	);
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="absolute" open={open}>
+        <Toolbar
+          sx={{
+            pr: "24px", // keep right padding when drawer closed
+          }}
+        >
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: "36px",
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Dashboard
+          </Typography>
+          <IconButton
+            color="inherit"
+            onClick={async () => {
+              navigate("/GET");
+            }}
+          >
+            <LogoutIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            px: [1],
+          }}
+        >
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          <NavLink to="/get">
+            <ListItemButton>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="get" />
+            </ListItemButton>
+          </NavLink>
+          <NavLink to="/post">
+            <ListItemButton>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="post" />
+            </ListItemButton>
+          </NavLink>
+          <NavLink to="/put">
+            <ListItemButton>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="put" />
+            </ListItemButton>
+          </NavLink>
+          <NavLink to="/patch">
+            <ListItemButton>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="patch" />
+            </ListItemButton>
+          </NavLink>
+          <NavLink to="/delete">
+            <ListItemButton>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="delete" />
+            </ListItemButton>
+          </NavLink>
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ m: 4 }}>
+          <Outlet />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <SignIn />,
-	},
-	{
-		element: <MainLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				path: "/dashboard",
-				element: <Dashboard />,
-			},
-			{
-				path: "/sportifs",
-				element: <Sportifs />,
-			},
-			{
-				path: "/coachs",
-				element: <Coachs />,
-			},
-			{
-				path: "/user/:uid",
-				element: <User />,
-			},
-		],
-	},
+  {
+    element: <MainLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Sportifs />,
+      },
+      {
+        path: "/get",
+        element: <Sportifs />,
+      },
+      {
+        path: "/post",
+        element: <Sportifs />,
+      },
+      {
+        path: "/put",
+        element: <Sportifs />,
+      },
+      {
+        path: "/patch",
+        element: <Sportifs />,
+      },
+      {
+        path: "/delete",
+        element: <Sportifs />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
